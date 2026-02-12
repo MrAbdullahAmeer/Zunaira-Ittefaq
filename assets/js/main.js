c7e76d3bd4ccd2b5534f09e6abbb4c1f828f611c
Abdullah Ameer <114295503+MrAbdullahAmeer@users.noreply.github.com>
2026-02-13 04:51:45 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,15 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-const navMenu = document.getElementById('nav-menu')
-
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navToggle){
+  navToggle.addEventListener('click', () =>{
+    navMenu.classList.add('show-menu')
+  })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
@@ -39,3 +44,4 @@ const navMenu = document.getElementById('nav-menu')
 
 
 /*==================== DARK LIGHT THEME ====================*/ 
+
