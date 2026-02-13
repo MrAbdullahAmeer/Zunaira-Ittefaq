5f7dccd37cc1d3a8c26f6743c22277a676fd67bf
Abdullah Ameer <114295503+MrAbdullahAmeer@users.noreply.github.com>
2026-02-13 05:01:30 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,10 +13,22 @@ if(navToggle){
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
+if(navClose){
+      navClose.addEventListener('click', () =>{
+            navMenu.classList.remove('show-menu')
+      })
+}
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
@@ -44,4 +56,5 @@ if(navToggle){
 
 
 /*==================== DARK LIGHT THEME ====================*/ 
+
 
